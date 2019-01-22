9825f86d740b6fc095106c2ed5402d48994f71ee
Edmund Ham <hamsu2959@gmail.com>
2019-01-22 10:32:02 -0800

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dotnet_db_test.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
